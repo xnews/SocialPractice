536179287b3c326f070fee1775949fad848a6647
cyxin <chenyixin648@163.com>
2021-11-05 22:41:24 +0800

    dev

--- examples/changePwd/changePwd.js
+++ examples/changePwd/changePwd.js
@@ -6,66 +6,68 @@ Page({
    */
   data: {
     form_list:[
-      {tagname: "手机号",name:"phonenumber"},
+      {tagname: "账号",name:"account"},
       {tagname: "新密码",name:"password"},
-      {tagname: "确认新密码",name:"compassword"},
-      {tagname: "短信验证码",name:"code"}
-    ]
+      {tagname: "确认新密码",name:"compassword"}
+    ],
+    inputValue: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 提交确认信息
+  changePwdSubmit(e) {
+    const {account, password, compassword} = e.detail.value    
+    const that = this
+    this.verifyCount(account).then(res =>{
+      if(res.length===0){
+        wx.showToast({
+          title: '账号不存在',
+          icon: 'error'
+        })
+      }else{
+        if(password!==compassword) {
+          wx.showToast({
+            title: '密码不一致',
+            icon: 'error'
+          })
+        }else{
+          that.changePassword(account,password)
+          this.setData({
+            inputValue: ""
+          })
+        }
+      }
+    })
   },
+  // 验证账号是否存在
+  verifyCount(count,password) {
+    return new Promise((resolve,reject) =>{
+      wx.cloud.callFunction({
+        name: 'getStuInfo',
+        data: {
+          stuNum: count
+        }
+      }).then(res =>{
+        resolve(res.result.data)
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 修改密码
+  changePassword(account,password) {
+    wx.cloud.callFunction({
+      name: 'updateStuPassword',
+      data: {
+        stuNum: account,
+        password
+      },
+      success: (res) =>{
+        wx.showToast({
+          title: '密码修改成功',
+          icon: 'success'
+        })
+      },
+      fail: (err) =>{
+        console.log('密码修改失败')
+      }
+    })
   }
 })
